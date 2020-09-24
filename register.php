529087a7dd4c5e91662138819f0b4b10f8c832f3
LukeB4UGame <44196194+LukeB4UGame@users.noreply.github.com>
2020-09-24 12:52:22 +0100

    Add files via upload

---
+++ register.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Forum</title>
+<meta charset='UTF-8'>
+<link rel="icon" type="image/gif/png" href="favicon.ico">
+</head>
+<body>
+<form action='register_parse.php' method='POST'>
+Username: <input type='text' name='username' placeholder='Username'/><br/>
+Password: <input type='password' name='password' placeholder='Password'/>
+<input type='submit' name='login' value='sign up'/><br/>
+</form>
+</body>
+</html>
